8707f04a451dac995c32d1cdbd55594c7640bc70
Sai Srikanth Madugula <saisrikanthmadugula@Sais-MacBook-Pro.local>
2020-05-03 08:49:34 +0530

    added new lines to the quotes.txt file using coding

--- get-quote.py
+++ get-quote.py
@@ -1,15 +1,34 @@
 import random
 
 def elementary():
-  print("Keep it logically awesome.")
+  print("Keep it logically awesome.\n")
 
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
 
-  last=13
+  last=len(quotes)-1
   rnd = random.randint(0,last)
   print(quotes[rnd])
+  rnd = random.randint(0,last)
+  print(quotes[rnd])
+
+  #creating a file object temp to write new lines to quotes.txt file.
+  temp=open("quotes.txt","a")
+  newquotes=["I am the best","We will win"]
+  for line in newquotes:
+    #write line to temp file
+    temp.write(line)
+    temp.write("\n")
+  temp.close()
+  
+  #printing the last two lines in the quotes.txt file
+  newtemp=open("quotes.txt")
+  quotes=newtemp.readlines()
+  last=len(quotes)-1
+  print(quotes[last-1])
+  print(quotes[last])
+  newtemp.close()
 
 if __name__== "__main__":
   elementary()
